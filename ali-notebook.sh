d1172d92cda4342837a440b2f55cdc742d19de92
chwg <chwg_frank@hotmail.com>
2024-06-02 19:39:18 +0800

    first commit

---
+++ ali-notebook.sh
@@ -0,0 +1,4 @@
+
+This is test line from local PC on 06-02
+
+Test TEST test
---
+++ linux-notebook.sh
@@ -0,0 +1,6 @@
+
+## This is my first line
+
+This line updated from remote Github
+
+This line updated from CWHG local PC
